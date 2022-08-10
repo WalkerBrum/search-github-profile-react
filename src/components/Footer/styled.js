64c100c64179889a89162f5d26306df2caf5176c
WalkerBrum <walker.b.lobato@gmail.com>
2022-08-10 02:05:37 -0300

    Adjusting css footer

--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -5,6 +5,7 @@ export const WrapperFooter = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    text-align: center;
     gap: 5px;
     background: black;
     color: white;
